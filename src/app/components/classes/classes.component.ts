1d4a70a93648ccbfd9dab02c89dcdec30d7ec040
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-20 16:54:27 -0500

    using async processes to change class names when they are over

--- src/app/components/classes/classes.component.ts
+++ src/app/components/classes/classes.component.ts
@@ -15,9 +15,24 @@ export class ClassesComponent implements OnInit {
 
   }
 
+  public loading: boolean = false; 
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public executeLoading() {
+
+    this.loading = true;
+
+    // Antes de dar clic al boton la variable loading esta en falso, luego cuando damos clic al boton la variable cambia a true, y permanece en true durante 3 segundos para finalmente tomar el valor de falso nuevamente 
+    setTimeout(() => {
+      
+      this.loading = false; 
+
+    }, 3000 );
+
   }
 
 }
