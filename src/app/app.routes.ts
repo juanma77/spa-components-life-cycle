5a57d4168720cfe902cf8b82b1946e255055a5a0
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-20 20:11:48 -0500

    child routes functionality added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./components/home/home.component";
 import { UserComponent } from './components/user/user.component';
+import { NewUserComponent } from './components/user/new-user.component';
+import { EditUserComponent } from './components/user/edit-user.component';
+import { UserDetailComponent } from './components/user/user-detail.component';
 
 const APP_ROUTES: Routes = [
 
     { path: 'home', component: HomeComponent },
-    { path: 'user', component: UserComponent },
+    { path: 'user/:id', component: UserComponent,
+
+    children: [
+
+        { path: 'new', component: NewUserComponent },
+        { path: 'edit', component: EditUserComponent },
+        { path: 'detail', component: UserDetailComponent },
+        { path: '**', pathMatch: 'full', redirectTo: 'edit' }
+    ]
+
+    },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
