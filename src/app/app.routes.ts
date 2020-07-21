8c5fb1dc9a55bf309b4020186455d7c3f1940fc6
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-21 12:39:48 -0500

    moving child routes to other file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from "./components/home/home.component";
 import { UserComponent } from './components/user/user.component';
-import { NewUserComponent } from './components/user/new-user.component';
-import { EditUserComponent } from './components/user/edit-user.component';
-import { UserDetailComponent } from './components/user/user-detail.component';
+import { USER_ROUTES } from './components/user/user.routes';
 
 const APP_ROUTES: Routes = [
 
     { path: 'home', component: HomeComponent },
-    { path: 'user/:id', component: UserComponent,
-
-    children: [
-
-        { path: 'new', component: NewUserComponent },
-        { path: 'edit', component: EditUserComponent },
-        { path: 'detail', component: UserDetailComponent },
-        { path: '**', pathMatch: 'full', redirectTo: 'edit' }
-    ]
+    { 
+        path: 'user/:id', 
+        component: UserComponent,
+        children: USER_ROUTES
 
     },
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
