f9ace4b8842e395e92eacb03e7980d22c0a5fa06
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-21 16:28:22 -0500

    passing params from a parent route to a child route

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: ActivatedRoute ) {
+
+    this.router.params.subscribe( parametros  =>{
+
+      console.log( parametros, 'RUTA PADRE' );
+
+    });
+
+
+  }
 
   ngOnInit() {
   }
